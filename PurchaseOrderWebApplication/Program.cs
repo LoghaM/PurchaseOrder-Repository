1f6f2dea011f0a6dbe22d21d570e366b38a88f9d
LoghaM <loghamathivanan19@gmail.com>
2024-02-13 14:28:06 +0530

    update program.cs

--- PurchaseOrderWebApplication/Program.cs
+++ PurchaseOrderWebApplication/Program.cs
@@ -1,46 +1,64 @@
 using Microsoft.AspNetCore.Authentication.Cookies;
 using PurchaseOrderWebApplication.Filters;
 using Serilog;
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateLogger();
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add Exception Filter
-builder.Services.AddScoped<ExceptionFilter>();
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-
-builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(option =>
+try
 {
-    option.LoginPath = "/Login/Login";
-    option.ExpireTimeSpan = TimeSpan.FromMinutes(5);
-});
-builder.Services.AddAuthentication();
-builder.Services.AddRazorPages();
-//Add support to logging with SERILOG
-builder.Host.UseSerilog((context, configuration) =>
-    configuration.ReadFrom.Configuration(context.Configuration));
+    Log.Information("Starting web application");
 
-var app = builder.Build();
+    var builder = WebApplication.CreateBuilder(args);
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    // Add Exception Filter
+    builder.Services.AddScoped<ExceptionFilter>();
+    //Add support to logging with SERILOG
+    builder.Host.UseSerilog((context, configuration) =>
+        configuration.ReadFrom.Configuration(context.Configuration));
+    // Add services to the container.
+    builder.Services.AddControllersWithViews();
+
+    builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(option =>
+    {
+        option.LoginPath = "/Login/Login";
+        option.ExpireTimeSpan = TimeSpan.FromMinutes(5);
+    });
+    builder.Services.AddAuthentication();
+    builder.Services.AddRazorPages();
+    
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Home/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
+
+    app.UseHttpsRedirection();
+    app.UseStaticFiles();
+    app.UseExceptionHandleMiddleware();
+    //Add support to logging request with SERILOG
+    app.UseSerilogRequestLogging();
+    
+    app.UseRouting();
+
+    app.UseAuthorization();
+
+    app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=PurchaseOrder}/{action=Index}");
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-//Add support to logging request with SERILOG
-app.UseSerilogRequestLogging();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=PurchaseOrder}/{action=Index}");
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
